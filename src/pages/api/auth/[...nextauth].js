8c30735583bafc816f54d7f0afd3364f60072305
Janic Holzherr <69592188+SomethingLikeAChicken@users.noreply.github.com>
2023-02-26 11:28:41 +0100

    login (not working)

---
+++ src/pages/api/auth/[...nextauth].js
@@ -0,0 +1,34 @@
+import GithubProvider from "next-auth/providers/github"
+
+export const authOptions = {
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    }),
+  ],
+  secret: "6OSjhyiDdJpcCjkxytaspMi1XQHChBRyRv1alG7uHp8=",
+  callbacks: {
+    session: async ({ session, token }) => {
+      if (session?.user) {
+        session.user.id = token.sub;
+      }
+      return session;
+    },
+    jwt: async ({ user, token }) => {
+      if (user) {
+        token.sub = user.id;
+      }
+      return token;
+    },
+
+  },
+  session: {
+    strategy: "jwt",
+    maxAge: 30 * 24 * 60 * 60,
+    updateAge: 24 * 60 * 60,
+    generateSessionToken: () => {
+        return randomUUID?.() ?? randomBytes(32).toString("hex")
+    }, 
+  },
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,5 @@
 import Head from "next/head";
-import Image from "next/image";
 import { Inter } from "@next/font/google";
-import Word from "./../js/word";
 import { useState, useEffect, useRef } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
